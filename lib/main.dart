7a121d27c4dd233bc69e1cd84e3669d0059fe760
princesszion <maikemvictorine@gmail.com>
2023-04-09 13:40:41 +0200

    modified files

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:miniproject2/screens/book_search_screen.dart';
 import 'package:miniproject2/screens/settings.dart';
 import 'package:miniproject2/screens/splash_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
+// 'package:firebase_core/firebase_core.dart';
 
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: Splash(),
+      home: MainPage(),
     );
   }
 }
